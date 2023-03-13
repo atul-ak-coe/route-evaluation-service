f2ea3ba5a43acc26b1c571cfb9f2999722d8f875
Atul Kumar <atul.ak@gmail.com>
2023-03-13 18:39:14 +0530

    Revert "Initial commit"
    This reverts commit b59ab56be22710147e63d8a5038c158683337e28.

--- src/main/java/com/fleet/management/routeevaluation/service/TrackingDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.fleet.management.routeevaluation.service;
-
-import com.fleet.management.routeevaluation.model.TrackingDetails;
-import reactor.core.publisher.Mono;
-
-public interface TrackingDetailsService {
-
-    Mono<TrackingDetails> processTrackingDetails(TrackingDetails trackingDetails);
-}
